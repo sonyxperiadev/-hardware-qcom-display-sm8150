0b9722de3b07c62064baa0967605803a57c3bceb
Pavel Dubrova <pashadubrova@gmail.com>
2024-04-03 16:07:12 +0300

    Revert "sdm: Add default implementation for IsDisplayHWAvailable()"
    This reverts commit 97ab9b05fae635fa89ab28405b92d935ef5713a7.
    
    Keep the interface with pure virtual functions.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- sdm/include/private/resource_interface.h
+++ sdm/include/private/resource_interface.h
@@ -143,7 +143,7 @@ class ResourceInterface {
   virtual uint32_t GetMixerCount() = 0;
   virtual void HandleTUITransition(Handle display_ctx, bool tui_active) = 0;
   virtual DisplayError SetBlendSpace(Handle display_ctx, const PrimariesTransfer &blend_space) = 0;
-  virtual bool IsDisplayHWAvailable() { return true; }
+  virtual bool IsDisplayHWAvailable() = 0;
 };
 
 }  // namespace sdm
